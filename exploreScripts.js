acf9028b1f0975d225549615ae149d1b1b3509bf
Jofred <71094875+JofredG@users.noreply.github.com>
2024-12-13 23:33:18 -0600

    initial commit: starter site

---
+++ exploreScripts.js
@@ -0,0 +1,9 @@
+
+// Typing Text Code
+const typed = new Typed('.multiple-text', {
+      strings: ['Guessing Game', 'Rather empty explore page' ],
+      typeSpeed: 80,
+      backSpeed: 80,
+      backDelay: 1200,
+      loop: true,
+    });
